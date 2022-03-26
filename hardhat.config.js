7e4805092c1f84618d874fbda2a284dcd1375ee2
Matthieu Auger <matthieua@theodo.fr>
2022-03-26 23:54:41 +0100

    verify contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,8 @@
 */
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
-const { API_URL, PRIVATE_KEY } = process.env;
+require("@nomiclabs/hardhat-etherscan");
+const { API_URL, PRIVATE_KEY, ETHERSCAN_KEY } = process.env;
 module.exports = {
    solidity: "0.8.1",
    defaultNetwork: "ropsten",
@@ -14,4 +15,9 @@ module.exports = {
          accounts: [`0x${PRIVATE_KEY}`]
       }
    },
+   etherscan: {
+      // Your API key for Etherscan
+      // Obtain one at https://etherscan.io/
+      apiKey: ETHERSCAN_KEY
+    }
 }
