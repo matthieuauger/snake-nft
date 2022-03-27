4719099b3f3b50cd7230f5e251b7a0af876bdd74
Matthieu Auger <matthieua@theodo.fr>
2022-03-27 16:10:04 +0200

    Base count = 0

--- test/SnakeNFT.test.js
+++ test/SnakeNFT.test.js
@@ -1,12 +1,19 @@
 const { expect } = require("chai");
 
 describe("Snake contract", function () {
-  it("Deployment should assign the total supply of tokens to the owner", async function () {
+  it("Deployment should name contract SnakeNFT", async function () {
+    const Token = await ethers.getContractFactory("SnakeNFT");
+    const hardhatToken = await Token.deploy();
+
+    expect(await hardhatToken.name()).to.equal("SnakeNFT");
+  });
+
+  it("Deployment should assign 0 supply to owner", async function () {
     const [owner] = await ethers.getSigners();
 
     const Token = await ethers.getContractFactory("SnakeNFT");
     const hardhatToken = await Token.deploy();
 
-    expect(await hardhatToken.name()).to.equal("SnakeNFT");
+    expect(await hardhatToken.balanceOf(owner.address)).to.equal(0);
   });
 });
