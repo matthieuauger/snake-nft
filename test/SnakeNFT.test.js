2074558f9cca8dd44bc0ce8f4b3cf752bd2b50d4
Matthieu Auger <matthieua@theodo.fr>
2022-03-27 22:40:05 +0200

    refactor: naming

--- test/SnakeNFT.test.js
+++ test/SnakeNFT.test.js
@@ -7,39 +7,39 @@ describe("Snake contract", function () {
   }
 
   it("Deployment should name contract SnakeNFT", async function () {
-    const hardhatToken = await getToken();
-    expect(await hardhatToken.name()).to.equal("SnakeNFT");
+    const snakeToken = await getToken();
+    expect(await snakeToken.name()).to.equal("SnakeNFT");
   });
   
   it("Minting should assign 1 nft to minter", async function () {
     const [owner] = await ethers.getSigners();
 
-    const hardhatToken = await getToken();
-    expect(await hardhatToken.balanceOf(owner.address)).to.equal(0);
+    const snakeToken = await getToken();
+    expect(await snakeToken.balanceOf(owner.address)).to.equal(0);
 
-    await hardhatToken.mint(owner.address);
+    await snakeToken.mint(owner.address);
 
-    expect(await hardhatToken.balanceOf(owner.address)).to.equal(1);
-    expect(await hardhatToken.ownerOf(1)).to.equal(owner.address);
-    expect(await hardhatToken.tokenURI(1)).to.equal("YOUR_API_URL/api/erc721/1");
+    expect(await snakeToken.balanceOf(owner.address)).to.equal(1);
+    expect(await snakeToken.ownerOf(1)).to.equal(owner.address);
+    expect(await snakeToken.tokenURI(1)).to.equal("YOUR_API_URL/api/erc721/1");
   });  
   
   it("Owner can change baseURI", async function () {
     const [owner] = await ethers.getSigners();
 
-    const hardhatToken = await getToken();
-    await hardhatToken.mint(owner.address);
-    expect(await hardhatToken.tokenURI(1)).to.equal("YOUR_API_URL/api/erc721/1");
+    const snakeToken = await getToken();
+    await snakeToken.mint(owner.address);
+    expect(await snakeToken.tokenURI(1)).to.equal("YOUR_API_URL/api/erc721/1");
 
-    await hardhatToken.setBaseTokenURI("YOUR_API_URL_2/api/erc721/");
-    expect(await hardhatToken.tokenURI(1)).to.equal("YOUR_API_URL_2/api/erc721/1");
+    await snakeToken.setBaseTokenURI("YOUR_API_URL_2/api/erc721/");
+    expect(await snakeToken.tokenURI(1)).to.equal("YOUR_API_URL_2/api/erc721/1");
   });
   
   it("Only Owner can change baseURI", async function () {
     const [, maliciousUser] = await ethers.getSigners();
 
-    const hardhatToken = await getToken();
-    const maliciousUserToken = await hardhatToken.connect(maliciousUser);
+    const snakeToken = await getToken();
+    const maliciousUserToken = await snakeToken.connect(maliciousUser);
 
     await maliciousUserToken.mint(maliciousUser.address);
     expect(await maliciousUserToken.tokenURI(1)).to.equal("YOUR_API_URL/api/erc721/1");
