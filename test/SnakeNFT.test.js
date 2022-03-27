c7ec8c7e2062398f89583cf918ee134dc55afe08
Matthieu Auger <matthieua@theodo.fr>
2022-03-27 16:13:31 +0200

    Test test

--- test/SnakeNFT.test.js
+++ test/SnakeNFT.test.js
@@ -7,15 +7,6 @@ describe("Snake contract", function () {
 
     expect(await hardhatToken.name()).to.equal("SnakeNFT");
   });
-
-  it("Deployment should assign 0 supply to owner", async function () {
-    const [owner] = await ethers.getSigners();
-
-    const Token = await ethers.getContractFactory("SnakeNFT");
-    const hardhatToken = await Token.deploy();
-
-    expect(await hardhatToken.balanceOf(owner.address)).to.equal(0);
-  });  
   
   it("Minting should assign 1 nft to minter", async function () {
     const [owner] = await ethers.getSigners();
@@ -23,8 +14,12 @@ describe("Snake contract", function () {
     const Token = await ethers.getContractFactory("SnakeNFT");
     const hardhatToken = await Token.deploy();
 
+    expect(await hardhatToken.balanceOf(owner.address)).to.equal(0);
+
     await hardhatToken.mint(owner.address);
 
     expect(await hardhatToken.balanceOf(owner.address)).to.equal(1);
+    expect(await hardhatToken.ownerOf(1)).to.equal(owner.address);
+    expect(await hardhatToken.tokenURI(1)).to.equal("YOUR_API_URL/api/erc721/1");
   });
 });
