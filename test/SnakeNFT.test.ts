745440d1ae8a2703fe7d9c9267489ed6eebe91f9
Matthieu Auger <matthieua@theodo.fr>
2022-03-31 09:10:12 +0200

    Reorganize code

--- test/SnakeNFT.test.ts
+++ test/SnakeNFT.test.ts
@@ -1,101 +1,86 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
+import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
-describe("Snake contract", function () {
-  type contractConfiguration = {
-    totalSupply: number;
-  }
+describe("Snake contract should allow users to mint ERC721 and owner to administrate", function () {
+  let snakeNFTContract: Contract;
+  let owner: SignerWithAddress;
+  let normalUser: SignerWithAddress;
 
-  async function deployAndGetToken(config: contractConfiguration) {
-    const snakeToken = await ethers.getContractFactory("SnakeNFT");
-    return await snakeToken.deploy("YOUR_API_URL/api/erc721/", config.totalSupply);
-  }
+  beforeEach(async () => {
+    const SnakeNFTFactory = await ethers.getContractFactory("SnakeNFT");
+    [owner, normalUser] = await ethers.getSigners();
+    snakeNFTContract = await SnakeNFTFactory.deploy(
+      "YOUR_API_URL/api/erc721/",
+      3
+    );
+  });
 
   it("Deployment should name contract SnakeNFT", async function () {
-    const snakeToken = await deployAndGetToken({totalSupply: 10});
-    expect(await snakeToken.name()).to.equal("SnakeNFT");
+    expect(await snakeNFTContract.name()).to.equal("SnakeNFT");
   });
   
   it("Minting should assign 1 nft to minter", async function () {
-    const snakeToken = await deployAndGetToken({totalSupply: 10});
-    
-    const [owner] = await ethers.getSigners();
-    expect(await snakeToken.balanceOf(owner.address)).to.equal(0);
+    expect(await snakeNFTContract.balanceOf(owner.address)).to.equal(0);
 
-    await snakeToken.mint(owner.address);
+    await snakeNFTContract.mint(owner.address);
 
-    expect(await snakeToken.balanceOf(owner.address)).to.equal(1);
-    expect(await snakeToken.ownerOf(1)).to.equal(owner.address);
-    expect(await snakeToken.tokenURI(1)).to.equal("YOUR_API_URL/api/erc721/1");
+    expect(await snakeNFTContract.balanceOf(owner.address)).to.equal(1);
+    expect(await snakeNFTContract.ownerOf(1)).to.equal(owner.address);
+    expect(await snakeNFTContract.tokenURI(1)).to.equal("YOUR_API_URL/api/erc721/1");
   });  
   
   it("Owner can change baseURI", async function () {    
-    const snakeToken = await deployAndGetToken({totalSupply: 10});
-    
-    const [owner] = await ethers.getSigners();
-    await snakeToken.mint(owner.address);
-    expect(await snakeToken.tokenURI(1)).to.equal("YOUR_API_URL/api/erc721/1");
+    await snakeNFTContract.mint(owner.address);
+    expect(await snakeNFTContract.tokenURI(1)).to.equal("YOUR_API_URL/api/erc721/1");
 
-    await snakeToken.setBaseTokenURI("YOUR_API_URL_2/api/erc721/");
-    expect(await snakeToken.tokenURI(1)).to.equal("YOUR_API_URL_2/api/erc721/1");
+    await snakeNFTContract.setBaseTokenURI("YOUR_API_URL_2/api/erc721/");
+    expect(await snakeNFTContract.tokenURI(1)).to.equal("YOUR_API_URL_2/api/erc721/1");
   });
   
   it("Only Owner can change baseURI", async function () {
-    const snakeToken = await deployAndGetToken({totalSupply: 10});
-    
-    const [, maliciousUser] = await ethers.getSigners();
-    const maliciousUserToken = await snakeToken.connect(maliciousUser);
+    snakeNFTContract = snakeNFTContract.connect(normalUser);
 
-    await maliciousUserToken.mint(maliciousUser.address);
-    expect(await maliciousUserToken.tokenURI(1)).to.equal("YOUR_API_URL/api/erc721/1");
+    await snakeNFTContract.mint(normalUser.address);
+    expect(await snakeNFTContract.tokenURI(1)).to.equal("YOUR_API_URL/api/erc721/1");
 
-    await expect(maliciousUserToken.setBaseTokenURI("YOUR_API_URL_2/api/erc721/"))
+    await expect(snakeNFTContract.setBaseTokenURI("YOUR_API_URL_2/api/erc721/"))
         .to.be.revertedWith('Ownable: caller is not the owner');
   });
 
   it("Can't mint more than supply", async function () {
-    const snakeToken = await deployAndGetToken({totalSupply: 3});
+    await snakeNFTContract.mint(owner.address);
+    await snakeNFTContract.mint(owner.address);
+    await snakeNFTContract.mint(owner.address);
     
-    const [owner] = await ethers.getSigners();
-    await snakeToken.mint(owner.address);
-    await snakeToken.mint(owner.address);
-    await snakeToken.mint(owner.address);
-    
-    await expect(snakeToken.mint(owner.address))
+    await expect(snakeNFTContract.mint(owner.address))
         .to.be.revertedWith("Total supply exceeded, no more available tokens");
   });
 
   it("Owner can pause contract", async function () {
-    const snakeToken = await deployAndGetToken({totalSupply: 3});
+    await snakeNFTContract.mint(owner.address);
+    await snakeNFTContract.pause();
     
-    const [owner] = await ethers.getSigners();
-    await snakeToken.mint(owner.address);
-    await snakeToken.pause();
-    
-    await expect(snakeToken.mint(owner.address))
+    await expect(snakeNFTContract.mint(owner.address))
         .to.be.revertedWith("Pausable: paused");      
   });  
   
   it("Owner can unpause contract", async function () {
-    const snakeToken = await deployAndGetToken({totalSupply: 3});
-    
     const [owner] = await ethers.getSigners();
-    await snakeToken.pause();
-    await snakeToken.unpause();  
-    await expect(snakeToken.mint(owner.address))
+    await snakeNFTContract.pause();
+    await snakeNFTContract.unpause();  
+    await expect(snakeNFTContract.mint(owner.address))
         .to.not.be.reverted;
   });
 
   it("Only Owner can pause and unpause contract", async function () {
-    const snakeToken = await deployAndGetToken({totalSupply: 10});
-    
-    const [, maliciousUser] = await ethers.getSigners();
-    const maliciousUserToken = await snakeToken.connect(maliciousUser);
+    snakeNFTContract = snakeNFTContract.connect(normalUser);
 
-    await expect(maliciousUserToken.pause())
+    await expect(snakeNFTContract.pause())
         .to.be.revertedWith('Ownable: caller is not the owner');
     
-    await expect(maliciousUserToken.unpause())
+    await expect(snakeNFTContract.unpause())
         .to.be.revertedWith('Ownable: caller is not the owner');
   });
 });
