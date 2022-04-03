b84bd42db564519675ee5bfff3823fe8dc9a9a73
Matthieu Auger <matthieua@theodo.fr>
2022-04-03 11:58:13 +0200

    Allow to set contract address in env

--- tasks/mint-nft.ts
+++ tasks/mint-nft.ts
@@ -1,13 +1,28 @@
 import "@nomiclabs/hardhat-web3";
 import { task } from "hardhat/config";
 
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+
 task("mint", "Mints a token")
-  .addParam("contract", "The contract address")
+  .addOptionalParam("contract", "The contract address")
   .setAction(async (taskArgs, { ethers }) => {
+
+    let contractAddress = taskArgs.contract;
+    if (typeof CONTRACT_ADDRESS !== "undefined") {
+      contractAddress = CONTRACT_ADDRESS;
+    }
+
+    if (contractAddress === undefined) {
+      console.error('Error: No contract address has been found either on environment or parameters, exiting');
+      
+      return;
+    }
+
+    console.log(`Minting NFT on ${contractAddress}`);
 
     const [owner] = await ethers.getSigners();
     const SnakeNFT = await ethers.getContractFactory('SnakeNFT');
-    const snakeNFT = SnakeNFT.attach(taskArgs.contract);
+    const snakeNFT = SnakeNFT.attach(contractAddress);
     await snakeNFT.mint(owner.address);
   });
 
